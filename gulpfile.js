4761b6188ef6862549c403b5fe9a3e6bd14b5552
Mark Odell <dracone.cordis@gmail.com>
2018-02-06 20:57:18 -0500

    Change footer background. Change title font to Open Sans. Fix min-height bug by using height.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
-gulp.task('dist', () =>
+gulp.task('dist', () => {
   gulp.src([
     'node_modules/normalize.css/normalize.css',
     'css/styles.css',
   ])
     .pipe(concat('mobius.css'))
-    .pipe(gulp.dest('./')));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
 
+gulp.task('dev', () => {
+  browserSync.init({
+    server: './',
+  });
+
+  gulp.watch('./css/*.css', ['dist']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['dev']);
