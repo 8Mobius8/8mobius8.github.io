33a643fdca8b35272341b08c4f1d893e33c8926d
8Mobius8 <m4rk.odell@gmail.com>
2024-11-24 02:02:58 -0600

    Update focus tracker

--- focus-tracker/index.service.worker.js
+++ focus-tracker/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732423839|5593232';
+const CACHE_VERSION = '1732435166|5489167';
 /** @type {string} */
 const CACHE_PREFIX = 'Focus Tracker-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
